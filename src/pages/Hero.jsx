8471cf4cfc1cda277f5f8f287b2522e2370d9846
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-07-29 23:37:15 -0300

    feat: add pages home and hero

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,173 @@
+import axios from 'axios';
+import md5 from 'md5';
+import {useEffect} from 'react';
+import {useLoaderData, useNavigate} from 'react-router-dom';
+import {useQuery} from 'react-query';
+import rateFill from '../assets/review/Path@2x.png';
+import rateEmpty from '../assets/review/Path Copy 6@2x.png';
+import {useState} from 'react';
+import Wrapper, {Navbar} from './styles/hero'
+import SearchForm from '../components/SearchForm';
+
+import logo from '../assets/logo/Group.png'
+import book from '../assets/icones/book/Group.png'
+import video from '../assets/icones/video/Shape.png'
+import fillHeart from '../assets/icones/heart/Path Copy 7@2x.png'
+
+// CONSTANTES PARA A API DA MARVEL
+const MARVEL_API_URL = 'http://gateway.marvel.com/v1/public/characters/';
+const TIME = Number(new Date());
+const HASH = md5(`${TIME}${import.meta.env.VITE_PRIVATE_KEY}${import.meta.env.VITE_PUBLIC_KEY}`);
+
+// FUNÇÃO PARA OBTER AS AVALIAÇÕES SALVAS NO LOCAL STORAGE
+const getRatingsFromLocalStorage = () => {
+  const storedRating = localStorage.getItem('ratings');
+  return storedRating ? JSON.parse(storedRating) : {};
+};
+
+// LOADER FUNCTION PARA OBTENÇÃO DOS DADOS DO HERÓI
+export const loader = async ({params}) => {
+  const {id} = params;
+  const {data} = await axios.get(
+    `${MARVEL_API_URL}${id}?ts=${TIME}&apikey=${import.meta.env.VITE_PUBLIC_KEY}&hash=${HASH}`
+  );
+  return {data, id};
+};
+
+const Hero = () => {
+
+  // OBTÉM O ID E OS DADOS DO HERÓI DO LOADER DATA
+  const {id, data} = useLoaderData();
+  const {results} = data.data;
+
+  // ESTADO PARA ARMAZENAR AS AVALIAÇÕES DOS HERÓIS
+  const [ratings, setRatings] = useState(getRatingsFromLocalStorage());
+  const [searchFormColor, setSearchFormColor] = useState('white');
+
+
+
+  // FUNÇÃO PARA LIDAR COM CLIQUES NA AVALIAÇÃO
+  const handleRateClick = (id, index) => {
+    setRatings((prevRatings) => {
+      const newRatings = {...prevRatings};
+      // Se a estrela clicada estiver igual à avaliação atual, então desmarcamos (empty)
+      if (newRatings[id] === index) {
+        newRatings[id] = index - 1;
+
+      } else {
+        // Caso contrário, atualizamos a avaliação para o valor correspondente ao índice da estrela clicada (full)
+        newRatings[id] = index;
+      }
+      return newRatings;
+    });
+  };
+
+  // ATUALIZA AS AVALIAÇÕES NO LOCAL STORAGE QUANDO O ESTADO "ratings" FOR ALTERADO
+  useEffect(() => {
+    localStorage.setItem('ratings', JSON.stringify(ratings));
+  }, [ratings]);
+
+  // FUNÇÃO ASSÍNCRONA PARA OBTER OS COMICS DO HERÓI
+  const getComics = async (id) => {
+    const response = await axios.get(
+      `${MARVEL_API_URL}${id}/comics?ts=${TIME}&apikey=${import.meta.env.VITE_PUBLIC_KEY}&hash=${HASH}&orderBy=-focDate&limit=10`
+    );
+    return response.data.data.results;
+  };
+
+  // REACT QUERY PARA OBTENÇÃO DOS COMICS DO HERÓI
+  const {
+    data: comicsData,
+    isLoading: isLoadingComics,
+    isError: isErrorComics,
+  } = useQuery(['comics', id], () => getComics(id));
+
+  return (
+    <section className='hero-page'>
+      <Navbar>
+        <div className="navbar">
+          <img src={logo} alt="" />
+          <div className="search">
+            <SearchForm color={searchFormColor} />
+          </div>
+        </div>
+      </Navbar>
+
+      <Wrapper>
+        {results.map((item) => {
+          const {thumbnail, description, name, series, comics} = item;
+          return (
+
+            <div key={id}>
+              <div className="hero-info">
+                <div className="hero-description">
+                  <div className="title">
+                    <h1>{name}</h1>
+                    <img src={fillHeart} alt="" />
+                  </div>
+                  <p className='description'>{description}</p>
+                  <div className="info">
+                    <div className="info-item">
+                      <h4>Quadrinhos</h4>
+                      <div className="icon">
+                        <img src={book} alt="" />
+                        <p>{comics.available}</p>
+                      </div>
+                    </div>
+                    <div className="info-item">
+                      <h4>Filmes</h4>
+                      <div className="icon">
+                        <img src={video} alt="" />
+                        <p>{series.available}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rating-container">
+                    <p>Rating: </p>
+                    {[1, 2, 3, 4, 5].map((index) => (
+                      <img
+                        key={index}
+                        onClick={() => handleRateClick(id, index)}
+                        src={index <= (ratings[id] || 0) ? rateFill : rateEmpty}
+                        alt=""
+                      />
+                    ))}
+                  </div>
+                </div>
+
+                <img
+                  className='img'
+                  src={`${thumbnail.path}/standard_xlarge.${thumbnail.extension}`}
+                  alt=""
+                />
+              </div>
+              <h2>Ultimos lançamentos</h2>
+
+              {isLoadingComics ? (
+                <div className="loading"></div>
+              ) : isErrorComics ? (
+                <div>Alguma coisa deu errado !</div>
+              ) : (
+                <section className='comics'>
+                  {comicsData.map((comic) => (
+                    <div key={comic.id}>
+                      <img
+                        src={`${comic.thumbnail.path}/standard_xlarge.${comic.thumbnail.extension}`}
+                        alt=""
+                      />
+                      <div>{comic.title}</div>
+                    </div>
+                  ))}
+                </section>
+
+              )}
+            </div>
+          );
+        })}
+        <footer></footer>
+      </Wrapper>
+    </section >
+  );
+};
+
+export default Hero;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,149 @@
+import axios from 'axios';
+import md5 from 'md5';
+import Characters from '../components/Characters';
+import {useLoaderData, useNavigation} from 'react-router-dom';
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+import SearchForm from '../components/SearchForm';
+import Wrapper from './styles/home'
+
+import logo from '../assets/logo/Group.png'
+
+import hero from '../assets/icones/heroi/noun_Superhero_2227044@2x.png'
+import toggleOn from '../assets/toggle/Group 2@2x.png'
+import toggleOff from '../assets/toggle/Group 6@2x.png'
+import emptyHeart from '../assets/icones/heart/Path Copy 2@2x.png'
+import fillHeart from '../assets/icones/heart/Path Copy 7@2x.png'
+import {useFavorites} from '../context/FavoriteContext';
+
+// CONSTANTES PARA A API DA MARVEL
+const MARVEL_API_URL = 'http://gateway.marvel.com/v1/public/characters?';
+const TIME = Number(new Date());
+const HASH = md5(`${TIME}${import.meta.env.VITE_PRIVATE_KEY}${import.meta.env.VITE_PUBLIC_KEY}`);
+const LIMIT = 20;
+
+// FUNÇÃO PARA OBTER A URL DE BUSCA COM BASE NOS PARÂMETROS
+const getSearchUrl = (searchTerm, offset, ascending) => {
+  const orderBy = ascending ? 'name' : '-name';
+  const params = new URLSearchParams({
+    ts: TIME,
+    apikey: import.meta.env.VITE_PUBLIC_KEY,
+    hash: HASH,
+    offset,
+    limit: LIMIT,
+    orderBy,
+  });
+
+  if (searchTerm) {
+    params.append('nameStartsWith', searchTerm);
+  }
+
+  return `${MARVEL_API_URL}${params.toString()}`;
+};
+
+// FUNÇÃO ASSÍNCRONA PARA BUSCAR HERÓIS NA API DA MARVEL
+const fetchHeroes = async (searchTerm, offset, ascending) => {
+  const url = getSearchUrl(searchTerm, offset, ascending);
+  const response = await axios.get(url);
+  return response.data.data.results;
+};
+
+// FUNÇÃO DE PAGINAÇÃO PARA BUSCAR HERÓIS COM BASE NO OFFSET
+const pagination = (offset) => {
+  return async (searchTerm) => {
+    const url = getSearchUrl(searchTerm, offset);
+    const response = await axios.get(url);
+    return response.data.data.results;
+  };
+};
+
+const Home = () => {
+  const {searchTerm} = useLoaderData();
+
+  const [searchFormColor, setSearchFormColor] = useState('#fdecec');
+
+  // ESTADOS PARA CONTROLE DE PAGINAÇÃO E ORDENAÇÃO
+  const [ascending, setAscending] = useState(true);
+  const [offset, setOffset] = useState(0);
+  const [toggle, setToggle] = useState(false)
+
+  const {favoritesList, showFavorites, setShowFavorites} = useFavorites();
+
+  // OBTÉM OS HERÓIS DA API UTILIZANDO REACT QUERY
+  const {data: heroes, isLoading, isError} = useQuery(
+    ['search', searchTerm, offset, ascending],
+    () => fetchHeroes(searchTerm, offset, ascending)
+  );
+
+  // FUNÇÕES PARA CONTROLE DE PAGINAÇÃO E ORDENAÇÃO
+  const handlePreviousPage = () => {
+    if (offset >= LIMIT) {
+      setOffset(offset - LIMIT);
+    }
+  };
+
+  const handleNextPage = () => {
+    setOffset(offset + LIMIT);
+  };
+
+  const handleSortAZ = () => {
+    setAscending(true);
+    setOffset(0);
+  };
+
+  const handleSortZA = () => {
+    setAscending(false);
+    setOffset(0);
+  };
+
+  return (
+    <>
+      <Wrapper>
+
+        <img className='logo' src={logo} alt="" />
+        <h4>EXPLORE O UNIVERSO</h4>
+        <p>mergulhe no dominio deslumbrante de todos os personagens classicos que você ama e aqueles que voce descobrira em breve !!</p>
+        <SearchForm color={searchFormColor} />
+        <div className="btns-container">
+          <span>Encontrados {showFavorites ? favoritesList.length : '20'} herois</span>
+          <div className="btn-item">
+            <img src={hero} alt="" />
+            <p>Ordenar por nome - A/Z</p>
+          </div>
+          <button onClick={toggle ? handleSortAZ : handleSortZA}>
+            <img id='btn-toggle' onClick={() => setToggle(!toggle)} src={toggle ? toggleOn : toggleOff} alt="" />
+          </button>
+          <div className="btn-item">
+            <button className='btn-fav' onClick={() => setShowFavorites((prev) => !prev)}>
+              <img src={showFavorites ? fillHeart : emptyHeart} alt="" />
+              {showFavorites ? <p>Mostrar Todos</p> : <p>Somente favoritos</p>}
+            </button>
+          </div>
+        </div>
+
+        <div className="pagination-container">
+          <button onClick={handlePreviousPage} disabled={offset === 0}>
+            PÁGINA ANTERIOR
+          </button>
+          <button onClick={handleNextPage}>
+            PRÓXIMA PÁGINA
+          </button>
+        </div>
+      </Wrapper>
+
+      {isLoading ?
+        (
+          <div className="load-container">
+            <div className="loading"></div>
+          </div>
+
+        ) : isError
+          ? (<div>ALGO DEU ERRADO...</div>)
+          : (<Characters data={heroes} />)
+      }
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
